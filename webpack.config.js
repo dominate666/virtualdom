a82429dacb2bc2df8cf598e43ef76e9ee562dd21
luofei <1063477160@qq.com>
2022-10-06 12:36:29 +0800

    添加忽略文件gitignore、添加虚拟目录

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path=require('path')
+const htmlWebpackPlugin=require('html-webpack-plugin')
+module.exports={
+    mode:"development",
+    entry:'./src/index.js',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:"bundle.js",
+        publicPath:'/xuni/'  //访问localhost:8080/xuni/bundle.js
+    },
+    plugins:[
+        new htmlWebpackPlugin({
+            filename:"test.html",
+            template:'www/index.html'
+        })
+    ],
+    devServer:{
+        port:8080,
+        open:true,
+        //静态资源文件夹
+        static:{
+            directory:'www'
+        }
+    }
+}
